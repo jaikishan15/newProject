07e1013ded1f2a22791965a71a4a5eb26de8d215
jaikishan15 <jai.kishan_cs19@gla.ac.in>
2020-08-22 17:12:16 +0530

    initialize variable

--- src/definitions/Video.java
+++ src/definitions/Video.java
@@ -11,5 +11,6 @@ public class Video {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("object-oriented programming");
+        int rollOfStudent = 24;
     }
 }
